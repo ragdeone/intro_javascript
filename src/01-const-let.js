1fa212d5d1643bc58ba385bd392343ee1cf9cce0
edgar nava <edgar.one@outlook.com>
2023-09-20 19:03:00 -0600

    primera practica

---
+++ src/01-const-let.js
@@ -0,0 +1,11 @@
+const nombre = 'edgar';
+const apellido = 'nava';
+
+let valorDado = 5;
+
+if (true) {
+    let valorDado = 6;
+    console.log(valorDado);
+}
+
+console.log(nombre, apellido, valorDado);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const nombre = 'edgar';
+const apellido =  'nava';
+
+const nombreCompleto = nombre +  ''+ apellido;
+console.log(nombreCompleto);
+
+const nombreCompleto2 = 'hola mundo';
+console.log(nombreCompleto2);
